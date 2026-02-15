317f9c52f341bad48786a75595182f5c1add3567
corkang <corkang19@gmail.com>
2026-02-16 01:31:43 +0900

    feat: 이삭줍기 로직 수정

--- server/models/user_lecture_gleaning_relation.js
+++ server/models/user_lecture_gleaning_relation.js
@@ -1,9 +1,14 @@
-const { Model } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
 class UserLectureGleaningRelation extends Model {
   static init(sequelize) {
     return super.init(
-      {},
+      {
+        emailSentCount: {
+          type: DataTypes.INTEGER,
+          defaultValue: 0,
+        },
+      },
       {
         tableName: 'UserLectureGleaningRelation',
         modelName: 'userLectureGleaningRelation',
